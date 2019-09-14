d3435887fb058fe6e642ea5c53010f5047f0f476
Kaitlyn <kaitlyn_l@rogers.com>
2019-09-14 15:11:50 -0400

    grabs topics

--- article_analyzer/article_extraction.py
+++ article_analyzer/article_extraction.py
@@ -1,3 +1,5 @@
+import operator
+
 from goose3 import Goose
 # Imports the Google Cloud client library
 from google.cloud import language
@@ -10,20 +12,61 @@ from google.cloud.language_v1beta2 import types as types_topic
 
 ## url from server which gets from chrome extension
 
-# urls = ['https://www.cbc.ca/news/politics/cabinet-confidence-trudeau-scheer-1.5283175',"https://www.bbc.com/news/science-environment-49567197", 'https://www.economist.com/leaders/2019/09/12/how-the-world-will-change-as-computers-spread-into-everyday-objects']
+urls = ['https://www.cbc.ca/news/politics/cabinet-confidence-trudeau-scheer-1.5283175',"https://www.bbc.com/news/science-environment-49567197", 'https://www.economist.com/leaders/2019/09/12/how-the-world-will-change-as-computers-spread-into-everyday-objects']
 # urls = ['https://www.foxnews.com/politics/pension-funds-in-iran-on-brink-of-collapse-amid-us-maximum-pressure-campaign']
-urls=['https://www.reddit.com/']
-article_data = []
-need_npl_topic = []
+# urls=['https://www.reddit.com/']
 
-with Goose() as g:
-    for link in urls:
-        article = g.extract(url=link)
-        ## opengraph gets author, source, title, topic (article:section), description and others, but some articles don't have everything
-        article_extraction = article.opengraph
-        article_extraction['cleaned_text'] = article.cleaned_text
-        article_data.append(article_extraction)
 
+def filter_articles(urls: list) -> list:
+    ## return list of dicts with the info
+    article_data = []
+    with Goose() as g:
+        for link in urls:
+            article = g.extract(url=link)
+            if article.opengraph['type'] == 'article':
+                ## opengraph gets author, source, title, topic (article:section), description and others, but some articles don't have everything
+                article_extraction = article.opengraph
+                article_extraction['cleaned_text'] = article.cleaned_text
+                article_data.append(article_extraction)
+    return article_data
+
+def filter_topic (topic_list: str) -> str:
+    topic = topic_list
+    ## this gets rid of first slash
+    topic = topic[1:]
+    ## this gets rid of stuff after the second slash
+    news_index = topic.find('News')
+    if news_index == 0:
+        topic = topic[5:]
+
+    slash_index = topic.find('/')
+    if slash_index != -1:
+        topic = topic[:slash_index]
+
+    return topic
+
+def get_topic(article_list: list):
+    ## add 'article:section' to object if it doesn't already have one. uses Cloud NLP
+    for article in article_list:
+        ## TOPIC IDENTIFIER
+        if 'article:section' not in article:
+            language_client = language_v1beta2.LanguageServiceClient()
+            document = types_topic.Document(
+                content=f"{article['cleaned_text']}",
+                type=enums_topic.Document.Type.PLAIN_TEXT
+            )
+            result = language_client.classify_text(document)
+            highest_confidence = []
+            for category in result.categories:
+                highest_confidence.append({'category': category.name, 'confidence': category.confidence})
+
+            highest = max(highest_confidence, key=lambda x: x['confidence'])
+            article['article:section'] = filter_topic(highest['category'])
+
+
+def get_sentiment(article_list: list):
+    ## gets sentiment analysis
+    for article in article_list:
         # Instantiates a client
         client = language.LanguageServiceClient()
 
@@ -35,23 +78,18 @@ with Goose() as g:
 
         # Detects the sentiment of the text
         sentiment = client.analyze_sentiment(document=document).document_sentiment
-        print(article_extraction["title"])
+        print(article["title"])
         print(article.opengraph['type'])
         print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
-        ## TOPIC IDENTIFIER
-        if 'article:section' not in article.opengraph:
-            language_client = language_v1beta2.LanguageServiceClient()
-            document = types_topic.Document(
-                content=f"{article_extraction['cleaned_text']}",
-                type=enums_topic.Document.Type.PLAIN_TEXT
-            )
-            result = language_client.classify_text(document)
-            for category in result.categories:
-                print('category name: ', category.name)
-                print('category confidence: ', category.confidence)
-                ## maybe take category with most confidence and the first of the slashed list
-        else:
-            print(article.opengraph['article:section'])
-                
-        print( )
+        print()
+
+
+def main():
+    articles = filter_articles(urls=urls)
+    get_topic(articles)
+
+
+if __name__ == "__main__":
+    main()
+
