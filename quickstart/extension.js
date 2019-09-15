85601aab988abf4ba81688d96dacf24040725642
Zoravur Singh <zoravur.s@gmail.com>
2019-09-15 07:18:25 -0400

    unhardcode pie chart

--- quickstart/extension.js
+++ quickstart/extension.js
@@ -42,10 +42,71 @@ fetch('http://localhost:8000/analysis').then(response => {
 
   Object.keys(data).forEach((key, i) => {
 
-    console.log('test')
-
-    container.appendChild(createProgressBar(key, data[key], colours[i]))
+    container.appendChild(createProgressBar(key, data[key].bias, colours[i]))
   })
+
+  registerPieChart(data)
 })
 
 colours = ['bg-success', 'bg-warning', 'bg-danger', 'bg-info']
+
+
+function registerPieChart(data) {
+  am4core.ready(function () {
+
+    am4core.useTheme(am4themes_animated);
+
+    var chart = am4core.create("chart-article", am4charts.PieChart);
+
+    // Add data
+    chart.data = Object.keys(data).map((key, i) => {
+      return {
+        "article": key,
+        "count": data[key].count
+      }
+    });
+
+
+
+    //   [{
+    //     "article": "Politics",
+    //     "count": 10
+    // }, {
+    //     "article": "Education",
+    //     "count": 10
+    // }, {
+    //     "article": "Technology",
+    //     "count": 6
+    // }, {
+    //     "article": "Environment",
+    //     "count": 9
+    // }];
+
+
+    chart.innerRadius = am4core.percent(35);
+
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "count";
+    pieSeries.dataFields.category = "article";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeWidth = 2;
+    pieSeries.slices.template.strokeOpacity = 1;
+
+    // This creates initial animation
+    pieSeries.hiddenState.properties.opacity = 1;
+    pieSeries.hiddenState.properties.endAngle = -90;
+    pieSeries.hiddenState.properties.startAngle = -90;
+
+    var colorSet = new am4core.ColorSet();
+    colorSet.list = ["#5cb85c", "#5bc0de", "#f0ad4e", "#d9534f"].map(function (color) {
+        return new am4core.color(color);
+    });
+    pieSeries.colors = colorSet;
+
+
+
+
+
+});
+}
